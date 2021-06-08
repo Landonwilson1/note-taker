372ad888006fb730c9f6c34b8841c7ad2891fe42
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2021-06-07 23:18:40 -0400

    Commit all Files to main

--- server.js
+++ server.js
@@ -1,58 +1,67 @@
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
-
-
+const fs = require("fs")
 const app = express();
+
 PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-
-//INDEX PAGE Home page
+//INDEX.HTML ROUTE
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-//NOTES page
-app.get("/notes", (req, res) =>{
-    fs.sendFile(path.join(__dirname, "/public/notes.html"))
+//NOTES.HTML ROUTE
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-
+//READS db.json RETURNS SAVED NOTES AS JSON
 app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8", (err, data) => {
-        if(err)
-        throw err
-        res.json(JSON.parse(data))
-    });
+    res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
+//POSTS NEW NOTE TO db.json RETURNS NEW NOTE TO CLIENT
+app.post("/api/notes", (req, res) => {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNotes = req.body;
 
-app.post("/api/notes/", (req,res ) => {
-    fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8", (err, data) => {
-        if(err)
-        throw err
+    savedNotes.push(newNotes);
 
-        const newNote = req.body;
-        var notes = JSON.parse(data);
-        notes.push(newNote);
-        fs.writeFile(".db/db.json", JSON.stringify(notes), function(err) {
-            if(err)
-            return console.log(err);
-        });
-    });   
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
 });
-
-
 
 //LISTENER
 app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
+    console.log(`App listening on PORT ${PORT} Woo!`);
 });
 
+
+
+/*
+Tried to make delete work 
+
+app.get("/api/notes/:id", (req, res) => {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.delete("/api/notes:id", (req, res) => { 
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = (req.params.id).toString();
+
+    savedNotes = savedNotes.filter(selected => {
+        return selected.id !=noteID;
+    })
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+});
+*/
 
 
 
